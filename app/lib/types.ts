44d1be43df0c98b4f8e2ea607e995f65fd7634c4
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-02-26 15:24:46 +0200

    Add CustomAuthError class

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -2,3 +2,10 @@ export type InitialErrors =
   | { email?: string[]; password?: string[]; fullName?: string[] }
   | string
   | undefined;
+
+export class CustomAuthError extends Error {
+  constructor(public data: InitialErrors, message?: string) {
+    super(message);
+    this.name = "CustomAuthError";
+  }
+}
