f0e8ae16e17d42d76195c329b0f742948281d571
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-02-26 15:37:51 +0200

    Add getUser function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,6 @@
+import { Prisma } from "@prisma/client/edge";
 import { z } from "zod";
+import prisma from "./prisma";
 
 export function formatZodErrors(errors: z.ZodError): Record<string, string[]> {
   const errorObject: Record<string, string[]> = {};
@@ -10,4 +12,21 @@ export function formatZodErrors(errors: z.ZodError): Record<string, string[]> {
   });
 
   return errorObject;
+}
+
+const findSpecificUser = (email: string) => {
+  return Prisma.validator<Prisma.UserWhereInput>()({
+    email,
+  });
+};
+
+export async function getUser(email: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: findSpecificUser(email),
+    });
+    return user;
+  } catch (error) {
+    throw new Error("Failed to fetch user.");
+  }
 }
