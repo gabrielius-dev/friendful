7214e069725b45aa39d11276fcdf99e884c0f2d1
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-02 12:06:37 +0200

    Add migrations

---
+++ prisma/migrations/20240302100229_/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "title",
+ADD COLUMN     "images" TEXT[];
