b1bc33119666bfb72349f0bb98a2cd112575207e
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-02 22:17:11 +0200

    Add PostSchema

--- app/lib/schemas.ts
+++ app/lib/schemas.ts
@@ -1,3 +1,4 @@
+import { File } from "buffer";
 import { z } from "zod";
 
 export const AuthSchema = z.object({
@@ -14,3 +15,47 @@ export const AuthSchema = z.object({
     .min(8)
     .max(100),
 });
+
+export const PostSchema = z
+  .object({
+    text: z.string().optional(),
+    images: z
+      .array(
+        z
+          .object({
+            size: z.number(),
+            type: z.string(),
+            name: z.string(),
+            lastModified: z.number(),
+          })
+          .refine(
+            (file) => {
+              if (file.size > 10 * 1024 * 1024) {
+                throw new Error(
+                  `File "${file.name}" exceeds the size limit of 10MB`
+                );
+              }
+              if (!file.type.startsWith("image/")) {
+                throw new Error(`File "${file.name}" is not of image type`);
+              }
+              return true;
+            },
+            {
+              message: "File validation failed",
+            }
+          )
+      )
+      .optional(),
+  })
+  .refine(
+    (data) => {
+      const { text, images } = data;
+      if (images?.length === 0) {
+        return typeof text === "string" && text.trim() !== "";
+      }
+      return true; // Text is optional if images array is not empty
+    },
+    {
+      message: "Text is required when images are empty",
+    }
+  );
