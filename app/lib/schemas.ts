10fb2d5f0887ffe2a9743c6a85cf40b2a957c952
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-15 22:31:24 +0200

    Split AuthSchema into SignUpSchema and LoginSchema

--- app/lib/schemas.ts
+++ app/lib/schemas.ts
@@ -1,9 +1,20 @@
 import { z } from "zod";
 
-export const AuthSchema = z.object({
+export const SignUpSchema = z.object({
   fullName: z
     .string({ required_error: "Full name must be specified" })
     .max(100),
+  email: z
+    .string({ required_error: "Email must be specified" })
+    .email("Invalid email")
+    .max(100),
+  password: z
+    .string({ required_error: "Password must be specified" })
+    .min(8)
+    .max(100),
+});
+
+export const LoginSchema = z.object({
   email: z
     .string({ required_error: "Email must be specified" })
     .email("Invalid email")
