1d36192d9fb9aa7a29a771e632bb9dadba9f8b95
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-16 22:36:21 +0200

    Add PostManager component

---
+++ app/ui/post/PostManager.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import StatusUpdate from "./StatusUpdate";
+import { useCallback, useEffect, useState } from "react";
+import { getCachedPosts } from "@/app/lib/serverUtils";
+import Post from "./Post";
+import { PrismaPost } from "@/app/lib/types";
+import { User } from "@prisma/client";
+import { useInView } from "react-intersection-observer";
+
+export default function PostManager({
+  currentUser,
+  initialPosts,
+}: {
+  currentUser: User;
+  initialPosts: PrismaPost[];
+}) {
+  const [posts, setPosts] = useState<PrismaPost[]>(initialPosts);
+  const [morePostsExist, setMorePostsExist] = useState(initialPosts.length > 1);
+  const [loading, setLoading] = useState(false);
+  const { ref, inView } = useInView();
+
+  const addPost = useCallback((post: PrismaPost) => {
+    setPosts((prevPosts) => [post, ...prevPosts]);
+  }, []);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+
+      const fetchedPosts = await getCachedPosts(currentUser.id, posts.length);
+
+      setPosts((prevPosts) => [...prevPosts, ...fetchedPosts]);
+      setLoading(false);
+
+      setMorePostsExist(fetchedPosts.length === 10);
+    };
+
+    if (inView) {
+      loadPosts();
+    }
+  }, [currentUser.id, inView, posts.length]);
+
+  const editPost = useCallback((postId: string, newPost: PrismaPost) => {
+    setPosts((prevPosts) =>
+      prevPosts.map((post) => {
+        if (post.id === postId) {
+          return newPost;
+        }
+        return post;
+      })
+    );
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-4 flex-grow max-w-2xl items-center mx-auto px-2 mb-2">
+      <StatusUpdate currentUser={currentUser} addPost={addPost} />
+      {posts.map((post) => (
+        <Post
+          post={post}
+          key={post.id}
+          currentUser={currentUser}
+          editPost={editPost}
+        />
+      ))}
+      {!loading && morePostsExist && <div ref={ref} />}
+    </div>
+  );
+}
