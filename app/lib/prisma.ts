97c44d1bb96028587f6f112bac29b4262108f8ab
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-16 18:31:53 +0200

    Update Prisma Client instantiation

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const createPrismaClient = () => new PrismaClient().$extends(withAccelerate());
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient>;
+};
+
+const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
