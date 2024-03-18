358395302653fe6fc8ddb4029f61ce064505cee2
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-18 21:02:37 +0200

    Add basic layout with NavigationBar and Main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <></>;
+import NavigationBar from "./ui/NavigationBar";
+import { currentUser } from "./lib/auth";
+import Main from "./ui/index/Main";
+import { Suspense } from "react";
+import { User } from "@prisma/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MainSkeleton from "./ui/index/MainSkeleton";
+
+export default async function Home() {
+  const user: User = (await currentUser())!;
+
+  return (
+    <div className="h-screen w-screen">
+      <NavigationBar currentUser={user} />
+      <div className="flex justify-between gap-4 mt-[72px]">
+        <Suspense fallback={<MainSkeleton />}>
+          <Main />
+        </Suspense>
+      </div>
+      <ToastContainer />
+    </div>
+  );
 }
