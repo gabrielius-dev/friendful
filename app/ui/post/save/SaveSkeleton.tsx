de24b25b9ec96fa8ef24486e42d8b360a70d94bd
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-03-22 21:20:01 +0200

    Add SaveSkeleton component

---
+++ app/ui/post/save/SaveSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@mui/material";
+
+export default function SaveSkeleton() {
+  return (
+    <div className="max-w-[600px] flex gap-2 items-center">
+      <Skeleton variant="circular" animation="pulse" width={40} height={40} />
+      <Skeleton
+        variant="text"
+        animation="pulse"
+        height={30}
+        sx={{ flex: 1, maxWidth: 390 }}
+      />
+    </div>
+  );
+}
